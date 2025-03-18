3e6539e80f081a1044d6d6b158aa20c63dffa3bd
Daniya Yelemessova <daniya@collingsmail.com>
2025-03-18 11:19:17 +0000

    Add quiz page

--- script.js
+++ script.js
@@ -1,3 +1,70 @@
+// ---quiz javascript----//
+const buttonn = document.querySelector('#btn');
+const answer = document.querySelector('#rightAnswer');
+const answer1 = document.querySelector('#rightAnswer1');
+const answer2 = document.querySelector('#rightAnswer2');
+const answer3 = document.querySelector('#rightAnswer3');
+const answer4 = document.querySelector('#rightAnswer4');
+buttonn.addEventListener('click', answers);
+
+
+function answers(e){
+    e.preventDefault();
+    let points = 0;
+    if(document.querySelector('#answer1').checked){
+        answer.textContent = 'Great! Your answer is right!';
+        answer.classList.add("right")
+        points++
+    }
+    else {
+        answer.textContent= 'Right answer is Harbour Bridge';
+        answer.classList.add("right")
+    }
+
+
+    if(document.querySelector('#answer2').checked){
+        answer1.textContent = 'Great! Your answer is right!';
+        answer1.classList.add("right")
+        points++
+    }
+    else {
+        answer1.textContent= 'Right answer is Victoria Peak';
+        answer1.classList.add("right")
+    }
+
+    if(document.querySelector('#answer3').checked){
+        answer2.textContent = 'Great! Your answer is right!';
+        answer2.classList.add("right")
+        points++
+    }
+    else {
+        answer2.textContent= 'Right answer is 93 meters';
+        answer2.classList.add("right")
+    }
+
+
+    if(document.querySelector('#answer4').checked){
+        answer3.textContent = 'Great! Your answer is right!';
+        answer3.classList.add("right")
+        points++
+    }
+
+    else {
+        answer3.textContent= 'Right answer is  good wildlife before rain starts ';
+        answer3.classList.add("right")
+    }
+
+
+    swal("Good job!", "Your points: " + points);
+
+
+
+} 
+
+
+
+
+
 // Select the first element with the class 'toggle-button' (used for the mobile menu toggle)
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 
